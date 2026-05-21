09cdb8774c435bec3a5ba5e2224ed45d08ac2544
Jan Medina <60261509+jan-medina@users.noreply.github.com>
2026-05-21 15:40:49 -0500

    Add deterministic SQLAlchemy naming convention

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,19 @@
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
 
+NAMING_CONVENTION = {
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s',
+}
+
 engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+Base = declarative_base(metadata=MetaData(naming_convention=NAMING_CONVENTION))
