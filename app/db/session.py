d02a7142bd70fac4d4e9e4e260838e9270a1e092
Jan Medina <60261509+jan-medina@users.noreply.github.com>
2026-05-20 23:39:57 -0500

    feat: add database session foundation

---
+++ app/db/session.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+
+engine = create_engine(settings.database_url)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
