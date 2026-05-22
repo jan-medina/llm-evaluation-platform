2fdbd7dc674e69f5ec46b35413b7fa5a0021a222
Jan Medina <60261509+jan-medina@users.noreply.github.com>
2026-05-21 19:44:44 -0500

    fix: make Alembic logging config optional

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,3 @@
-from logging.config import fileConfig
-
 from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -10,10 +8,6 @@ from app.db.session import Base
 
 config = context.config
 config.set_main_option('sqlalchemy.url', settings.database_url)
-
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
 
 target_metadata = Base.metadata
 
